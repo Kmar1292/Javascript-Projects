ae760b4e532b9d81bbb11b6e203f4395842621ca
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-05-31 15:30:07 -0700

    Create JS Project 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("BOO!");  //window alert pop up saying BOO!f
+
+var Sent1 = "This is the beginning of the string";  //created variable Sent1
+var Sent2 = " and this is the end of the string";  //created variable Sent2
+
+document.write(Sent1 + Sent2);  //concatenate and display the strings Sent1 and Sent2
+
+var age = 2023 - 1992; //set the variable age equal to the expression 2023-1992
+
+document.write(age);  //display the variable age
