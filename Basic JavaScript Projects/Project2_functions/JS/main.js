e839bc1b36c67cc68039d0b73a582d8c6dd81351
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-05-31 16:28:34 -0700

    update comments on main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -4,8 +4,8 @@ function displayText(){
     document.getElementById("display_text").innerHTML = txt1;   //display txt1 for elements with ID "display_text"
 }
 
-function displayText2(){
+function displayText2(){    //create second function for <p> element
     var txt1 = "This is ";  //create variable txt1
     txt1 += "some other text.";   //concatenate txt1 with an additional string using the += operator
-    document.getElementById("display_text2").innerHTML = txt1;   //display txt1 for elements with ID "display_text"
+    document.getElementById("display_text2").innerHTML = txt1;   //display txt1 for elements with ID "display_text2"
 }
