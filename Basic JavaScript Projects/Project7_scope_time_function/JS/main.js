9c67e9620eab9bebc66406f38d8f4bb04de94d74
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-06-02 00:58:25 -0700

    create JS project 7

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,34 @@
+var x = 3;      //assign global variable x
+
+function addTen(num){       //function to add 10 to a number
+    var y = 10;     //assign local variable y
+    return num + y;
+}
+
+document.write(addTen(x));
+
+function multiplyTen(num){      //function to multiply 10 to a number
+    return num * y;   
+}
+
+console.log(multiplyTen(x));        //display x * 10 in console, however there should be an error since it uses a local variable it doesnt have access to 
+
+
+function Time_function(){       
+    var Time = new Date().getHours();       //set Time variable to current hour
+    var Reply;
+
+    if (Time < 12 || Time == 0){        //if time is less than 12pm or is equal to 12am
+        Reply = "It is morning time!";
+    }
+
+    else if (Time >= 12 && Time < 18){      //if it is not morning, check if the time is between 12pm and 6pm
+        Reply = "It is afternoon.";
+    }
+
+    else{       //if it is not morning or afternoon
+        Reply = "It is evening time.";
+    }
+
+    document.getElementById("Time_of_day").innerHTML = Reply;       //display if it is morning, afternoon, or evening.
+}
